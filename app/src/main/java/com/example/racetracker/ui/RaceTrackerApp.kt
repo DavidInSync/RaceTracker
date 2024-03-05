7305f78e5c0f8c8dfe33e41f018a5f99bdf5eb0b
David <david.noctis99@gmail.com>
2024-03-05 19:34:42 +0100

    Iniciar dos corrutinas a la vez.

--- app/src/main/java/com/example/racetracker/ui/RaceTrackerApp.kt
+++ app/src/main/java/com/example/racetracker/ui/RaceTrackerApp.kt
@@ -52,6 +52,8 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.racetracker.R
 import com.example.racetracker.ui.theme.RaceTrackerTheme
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
 
 @Composable
 fun RaceTrackerApp() {
@@ -68,9 +70,14 @@ fun RaceTrackerApp() {
     }
     var raceInProgress by remember { mutableStateOf(false) }
 
-    LaunchedEffect(playerOne, playerTwo) {
-        playerOne.run()
-        playerTwo.run()
+    if (raceInProgress) {
+        LaunchedEffect(playerOne, playerTwo) {
+            coroutineScope {
+                launch { playerOne.run() }
+                launch { playerTwo.run() }
+            }
+            raceInProgress = false
+        }
     }
 
     RaceTrackerScreen(
